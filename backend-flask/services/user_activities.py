02383ec3e6d6d1e88a30c6364cf164fc3b6000cc
Khayrullo Isomiddinov <isomiddinovkhayrullo@gmail.com>
2026-09-05 12:41:49 +0200

    instrument xray

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,23 +1,29 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
-    model = {
-      'errors': None,
-      'data': None
-    }
+    with xray_recorder.in_subsegment('user_activities_run') as subsegment:
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    now = datetime.now(timezone.utc).astimezone()
+      now = datetime.now(timezone.utc).astimezone()
+      subsegment.put_annotation('user_handle', user_handle)
 
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
-    else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
-    return model
+      if user_handle == None or len(user_handle) < 1:
+        model['errors'] = ['blank_user_handle']
+        subsegment.put_metadata('model', model)
+      else:
+        now = datetime.now()
+        results = [{
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Andrew Brown',
+          'message': 'Cloud is fun!',
+          'created_at': (now - timedelta(days=1)).isoformat(),
+          'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
+        subsegment.put_metadata('model', model)
+      return model
